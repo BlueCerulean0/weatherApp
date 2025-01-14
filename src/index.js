d3c771a1e08986c5730948ae8fc0ffc07660cc68
Roy <126029801+BlueCerulean0@users.noreply.github.com>
2025-01-14 15:09:19 +0000

    Basics done. UI left.

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import './style.css';
+
+const key= "AT5J3QEY95FF7MDY8JAR9QXDF";
+
+const form = document.querySelector('form');
+const cityInput = document.querySelector('.city');
+const submit = document.querySelector('.submit');
+const result = document.querySelector('.result');
+
+console.log("what")
+submit.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    let Timezone;
+    let Temperature;
+    let FeelsLike;
+    let Humidity;
+    let WindSpeed;
+    let DewPoint;
+    let Pressure;
+
+    async function getWeather() {
+        
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${key}`, {mode: 'cors'});
+
+        const wetherData = await response.json();
+        console.log(wetherData)
+
+        Timezone = wetherData.timezone;
+        Temperature = wetherData.currentConditions.temp;
+        FeelsLike = wetherData.currentConditions.feelslike;
+        Humidity = wetherData.currentConditions.humidity;
+        WindSpeed = wetherData.currentConditions.windspeed;
+        DewPoint = wetherData.currentConditions.dew;
+        Pressure = wetherData.currentConditions.pressure;
+        
+        await render();
+    }
+
+    function render() {
+        result.innerHTML = ""
+
+        result.innerHTML = `
+
+        <h1>Timezone: ${Timezone}</h1>
+        <h1>Temperature: ${Temperature}</h1>
+        <h1>Feels like: ${FeelsLike}</h1>
+        <h1>Humidity: ${Humidity}</h1>
+        <h1>Wind Speed: ${WindSpeed}</h1>
+        <h1>Dew Point: ${DewPoint}</h1>
+        <h1>Pressure: ${Pressure}</h1>
+    `
+    }
+
+    await getWeather();
+
+    
+})
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        open: true,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ]
+}
